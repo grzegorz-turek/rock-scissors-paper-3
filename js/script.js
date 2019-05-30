14eec3416259c984eb8f6b89c5245329cebb537d
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-05-30 12:15:40 +0200

    Final

--- js/script.js
+++ js/script.js
@@ -13,12 +13,16 @@
     var roundWonByComputerNumber = 0;
     var roundWonByPlayerNumber = 0;
     var roundToWinNumber = 0;
+    var roundWinner = 0;
+    var computerMove = 0;
 
     var params = {
         roundNumber: roundNumber,
         roundWonByComputerNumber: roundWonByComputerNumber,
         roundWonByPlayerNumber: roundWonByPlayerNumber,
         roundToWinNumber: roundToWinNumber,
+        roundWinner: roundWinner,
+        computerMove: computerMove,
         progress: []
     };
 
@@ -27,6 +31,7 @@
         params.roundNumber = 0;
         params.roundWonByComputerNumber = 0;
         params.roundWonByPlayerNumber = 0;
+        params.progress = [];
         output.innerHTML = 'You started a New Game';
         finalResult.innerHTML = '';
         display();
@@ -37,26 +42,22 @@
             pleasePressTheNewGameButton();
             return;
         } 
-        var computerMove = newGetRandomIntInclusive();
-        if ((computerMove == 'rock' && playerMove == 'scissors') || (computerMove == 'scissors' && playerMove == 'paper') || (computerMove == 'paper' && playerMove == 'rock')) {
+        params.computerMove = newGetRandomIntInclusive();
+        if ((params.computerMove == 'rock' && playerMove == 'scissors') || (params.computerMove == 'scissors' && playerMove == 'paper') || (params.computerMove == 'paper' && playerMove == 'rock')) {
             output.innerHTML = 'ROUND LOST';
             params.roundWonByComputerNumber++;
-        } else if (computerMove == playerMove) {
+            params.roundWinner = 'computer';
+        } else if (params.computerMove == playerMove) {
             output.innerHTML = 'TIE';
+            params.roundWinner ='tie'
         } else {
             output.innerHTML = 'YOU WON';
             params.roundWonByPlayerNumber++;
+            params.roundWinner = 'you'
         }
         roundCount();
+        addGameStats(playerMove);
         winnerWonEntireGameDisplay();
-        addGameStats();
-
-
-        console.log('computerMove:', computerMove, 'playerMove:', playerMove); // TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-        console.log('params:', params); // TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-        console.log('params.roundNumber:', params.roundNumber, 'params.roundWonByComputerNumber:', params.roundWonByComputerNumber, 'params.roundWonByPlayerNumber:', params.roundWonByPlayerNumber, 'params.roundToWinNumber:', params.roundToWinNumber); // TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST
-        console.log('params.progress:', params.progress);
-//        console.log('params.progress[0]:', params.progress[0]);
     }
 
     var buttonClick = document.querySelectorAll('.player-move');
@@ -67,18 +68,15 @@
         });
     });
 
-    function addGameStats () {
+    function addGameStats(playerMove) {
         params.progress.push({
-            proundNumber: params.roundNumber,
-            proundWonByComputerNumber: params.roundWonByComputerNumber,
-            proundWonByPlayerNumber: params.roundWonByPlayerNumber,
-            proundResult: 'x',
-            pgameResult: 'y'
+            keyRoundNumber: params.roundNumber,
+            keyComputerMove: params.computerMove,
+            keyPlayerMove: playerMove,
+            keyRoundWinner: params.roundWinner,
+            keyGameResult: (params.roundWonByPlayerNumber + ' - ' + params.roundWonByComputerNumber)
         });
     }
-
-
-
 
     function pleasePressTheNewGameButton() {
         finalResult.innerHTML += 'Game over, please press the new game button !!!' + '<br>';
@@ -88,10 +86,13 @@
         if (params.roundToWinNumber == params.roundWonByComputerNumber) {
             output.innerHTML += ', COMPUTER WON ENTIRE GAME !!!';
             showModalOne();
+            document.getElementById('tablemessage').innerHTML = 'Computer won entire game!';
+
         }
         if (params.roundToWinNumber == params.roundWonByPlayerNumber) {
             output.innerHTML += ', YOU WON ENTIRE GAME !!!';
             showModalOne();
+            document.getElementById('tablemessage').innerHTML = 'You won entire game!';
         }
     }
 
@@ -126,23 +127,68 @@
     }
 
 	var showModalOne = function(){
-		event.preventDefault();
         document.querySelector('#modal-overlay').classList.add('show');
         document.querySelector('#modal-one').classList.add('show');
+        showGameStats();
     };
+
 	var hideModal = function(event){
-		event.preventDefault();
 		document.querySelector('#modal-overlay').classList.remove('show');
-	};
+    };
+    
 	var closeButtons = document.querySelectorAll('.modal .close');
 	for(var i = 0; i < closeButtons.length; i++){
 		closeButtons[i].addEventListener('click', hideModal);
-	}
+    }
+    
 	document.querySelector('#modal-overlay').addEventListener('click', hideModal);
 	var modals = document.querySelectorAll('.modal');
 	for(var i = 0; i < modals.length; i++){
 		modals[i].addEventListener('click', function(event){
 			event.stopPropagation();
 		});
-	}
+    }
+    
+    function showGameStats() {
+        var mainDiv = document.getElementById('modalcontent');
+        mainDiv.innerHTML = '';
+        var divContainer = document.createElement('div');
+        mainDiv.appendChild(divContainer);
+        divContainer.setAttribute('class', 'table__row');
+        var divRoundNumber = document.createElement('div');
+        divContainer.appendChild(divRoundNumber);
+        divRoundNumber.innerHTML += 'Round Number';
+        var divPlayerMove = document.createElement('div');
+        divContainer.appendChild(divPlayerMove);
+        divPlayerMove.innerHTML += 'Player Move';
+        var divComputerMove = document.createElement('div');
+        divContainer.appendChild(divComputerMove);
+        divComputerMove.innerHTML += 'Computer Move';
+        var divRoundWinner = document.createElement('div');
+        divContainer.appendChild(divRoundWinner);
+        divRoundWinner.innerHTML += 'Round Winner';
+        var divGameResult = document.createElement('div');
+        divContainer.appendChild(divGameResult);
+        divGameResult.innerHTML += 'Score';
+        for (var i = 0; i < params.progress.length; i++) {
+            var divContainer = document.createElement('div');
+            mainDiv.appendChild(divContainer);
+            divContainer.setAttribute('class', 'table__row');
+            var divRoundNumber = document.createElement('div');
+            divContainer.appendChild(divRoundNumber);
+            divRoundNumber.innerHTML += params.progress[i].keyRoundNumber;
+            var divPlayerMove = document.createElement('div');
+            divContainer.appendChild(divPlayerMove);
+            divPlayerMove.innerHTML += params.progress[i].keyPlayerMove;
+            var divComputerMove = document.createElement('div');
+            divContainer.appendChild(divComputerMove);
+            divComputerMove.innerHTML += params.progress[i].keyComputerMove;
+            var divRoundWinner = document.createElement('div');
+            divContainer.appendChild(divRoundWinner);
+            divRoundWinner.innerHTML += params.progress[i].keyRoundWinner;
+            var divGameResult = document.createElement('div');
+            divContainer.appendChild(divGameResult);
+            divGameResult.innerHTML += params.progress[i].keyGameResult;            
+        };
+    };
 })(); 
